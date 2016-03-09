b10be1cab86b628327ee72ebd79aedcfa180489a
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2016-03-09 17:56:47 +0000

    Calculate metrics

--- src/righter/analyse.py
+++ src/righter/analyse.py
@@ -49,12 +49,16 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--annotated-file', help='Annotated input file', required=True)
     parser.add_argument('-p', '--predicted-file', help='Predicted input file', required=True)
     parser.add_argument('-m', '--mistake-type', help='Type of mistake', choices=['AR', 'C', 'SP', 'NSW'])
-    parser.add_argument('-t', '--analysis-type', help='Type of analysis', default='quantitative', choices=['quantitative', 'qualitative'])
+    parser.add_argument('-t', '--analysis-type', help='Type of analysis', choices=['quantitative', 'qualitative'])
     parser.add_argument('-o', '--file-output', help='Save analysis to output file')
     args = parser.parse_args()
 
     annotated = read_writings(args.annotated_file, args.mistake_type)
     predicted = read_writings(args.predicted_file, args.mistake_type)
 
-    for key, changes in annotated.items():
-        print(key, changes)
+    if args.analysis_type == 'quantitative':
+        print('Precision:', precision(annotated, predicted))
+        print('Recall:', recall(annotated, predicted))
+    else:
+        for key, changes in annotated.items():
+            print(key, changes)
