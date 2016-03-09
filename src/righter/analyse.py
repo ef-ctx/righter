ddc1abb6fa32496e509d02aaece44eb86a76a30d
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2016-03-09 18:12:07 +0000

    Add qualitative analysis (comparison table)

--- src/righter/analyse.py
+++ src/righter/analyse.py
@@ -1,23 +1,66 @@
 import argparse
 import json
+import textwrap
+from terminaltables import AsciiTable
 
 
-def filter_changes(changes, mistake_type):
+TEXTWRAP = 80
+
+
+def filter_changes(writing, mistake_type):
     if mistake_type is not None:
-        return [c for c in changes if c["symbol"] == mistake_type]
-    else:
-        return changes
+        writing["changes"] = [i for i in writing["changes"] if i["symbol"] == mistake_type]
 
 
 def read_writings(file_name, mistake_type):
-    writings = {}
+    writings = []
     with open(file_name, 'r') as fp:
         for line in fp:
             writing = json.loads(line.strip())
-            changes = filter_changes(writing['changes'], mistake_type)
-            if changes:
-                writings[writing['id']] = changes
+            filter_changes(writing, mistake_type)
+            if writing.get("changes"):
+                writings.append(writing)
     return writings
+
+
+def map_id_to_field(writings, field):
+    writings_dict = {}
+    for writing in writings:
+        writings_dict[writing['id']] = writing[field]
+    return writings_dict
+
+
+def format_changes(original_changes):
+    changes = []
+    for i in original_changes:
+        xubi = "{} ({})".format(i["selection"], i["start"])
+        changes.append(xubi)
+    return "\n".join(changes)
+
+
+def format_text(text):
+    chunks = textwrap.wrap(text, TEXTWRAP)
+    return "\n".join(chunks)
+
+
+def show_comparison(baseline, predicted):
+    data = []
+    headers = ["id", "text", "baseline", "predicted"]
+    data.append(headers)
+    
+    baseline_dict = map_id_to_field(baseline, "changes")
+    predicted_dict = map_id_to_field(predicted, "changes")
+    writings_dict = map_id_to_field(baseline, "text")
+
+    for id_, text in writings_dict.items():
+        text = format_text(text)
+        baseline = format_changes(baseline_dict[id_])
+        predicted = format_changes(predicted_dict[id_])
+        row = [id_, text, baseline, predicted]
+        data.append(row)
+    table = AsciiTable(data)
+    table.inner_row_border = True
+    print(table.table)
 
 
 if __name__ == "__main__":
@@ -31,6 +74,5 @@ if __name__ == "__main__":
 
     annotated = read_writings(args.annotated_file, args.mistake_type)
     predicted = read_writings(args.predicted_file, args.mistake_type)
+    show_comparison(annotated, predicted)
 
-    for key, changes in annotated.items():
-        print(key, changes)
