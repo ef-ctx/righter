738c0d1dac8b673ef58c144880129f4ef9505845
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2016-04-08 09:24:05 +0100

    Use ginger when doing predict

--- src/righter/competitor/ginger.py
+++ src/righter/competitor/ginger.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import quote_plus
 
 
 TYPE_MAP = {
@@ -7,26 +8,49 @@ TYPE_MAP = {
 }
 
 
+class HTTPError(Exception):
+    pass
+
+
 def check(text):
     """Given a text, query ginger API and return a list of dicts in the same
     format as righter.check"""
     changes = []
 
-    url = 'https://services.gingersoftware.com/Ginger/correct/jsonSecured/GingerTheTextFull?apiKey=GingerWebSite&lang=US&clientVersion=2.0&text={}&_=1459928621895'
-    response = requests.get(url.format(text)).json()
-    for correction in response["Corrections"]:
-        change = {}
-        type_ = TYPE_MAP[correction["Type"]]        
-        word = text[correction["From"]:correction["To"] + 1]
-        if type_ == 'SP' and any(c['Text'].lower() == word for c in correction['Suggestions']):
-            type_ = 'C'
-        change = {
-            'selection': word,
-            'start': correction['From'],
-            'symbol': type_,
-        }
-        if correction['Suggestions']:
-            change['correction'] = correction['Suggestions'][0]['Text']
-        changes.append(change)
+    url = 'https://services.gingersoftware.com/Ginger/correct/jsonSecured/GingerTheTextFull'
+    params = {
+        "text": text,
+        "isOnTheFly": "false",
+        "securedPage": "true",
+        "apiKey": "BrowserStandalone",
+        "lang": "US",
+        "clientVersion": "0.1.0.562",
+        "_": "1460102265093"
+    }
+    # Ginger usually splits texts on sentences. So, if you have a really big
+    # sentence, even ginger plugin will fail. It does make sense to split by
+    # sentences because there are sentence-wide analysis that could be made.
+    resp = requests.get(url, params=params)
+    if not resp.ok:
+        raise HTTPError()
+    response = resp.json()
+    try:
+        for correction in response["Corrections"]:
+            change = {}
+            type_ = TYPE_MAP[correction["Type"]]
+            word = text[correction["From"]:correction["To"] + 1]
+            if type_ == 'SP' and any(c['Text'].lower() == word for c in correction['Suggestions']):
+                type_ = 'C'
+            change = {
+                'selection': word,
+                'start': correction['From'],
+                'symbol': type_,
+            }
+            if correction['Suggestions']:
+                change['correction'] = correction['Suggestions'][0]['Text']
+            changes.append(change)
+    except:
+        import pdb; pdb.set_trace()
+        raise
 
     return changes
--- src/righter/predict.py
+++ src/righter/predict.py
@@ -2,16 +2,23 @@ import json
 import argparse
 
 import righter
+from righter.competitor import ginger
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', help='File with one json per line containing the key text', required=True)
     parser.add_argument('-o', '--file-output', help='Save analysis to output file', required=True)
+    parser.add_argument('-a', '--algorithm', help='Name of the algorithm to use')
     args = parser.parse_args()
+
+    if args.algorithm is None:
+        check = righter.check
+    elif args.algorithm == 'ginger':
+        check = ginger.check
 
     with open(args.input_file, 'r') as input_fp:
         with open(args.file_output, 'w') as output_fp:
             for line in input_fp:
                 writing = json.loads(line.strip())
-                writing['changes'] = righter.check(writing['text'])
+                writing['changes'] = check(writing['text'])
                 print(json.dumps(writing), file=output_fp)
