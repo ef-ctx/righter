684269c8330101d7627cc05a4a65b2b06cede46e
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2016-04-11 07:13:15 +0100

    Add ugly but functional grammarly code

--- src/righter/competitor/grammarly.py
+++ src/righter/competitor/grammarly.py
@@ -1,18 +1,22 @@
+"""
+Script which makes calls to Grammarly, using websocket, and return the mistakes analysis
+http://grammarly.com/
+"""
 import json
+import ssl
+import time
+from datetime import datetime
+
 import websocket
 
+connection = None
 
 cookie = "optimizelyEndUserId=oeu1459928410487r0.9973470086697489; _mhbkts=gdn; _gat=1; grauth=AABCAbFtEQyApg22JHd-rVzeQehX0nOd5Awy46c96sRt9UCm2BxttgZT4NZ7-7l548g9Ig; redirect_location=eyJ0b2tlbiI6IkFBQkNBYkZ0RVF5QXBnMjJKSGQtclZ6ZVFlaFgwbk9kNUF3eTQ2Yzk2c1J0OVVDbTJCeHR0Z1pUNE5aNy03bDU0OGc5SWciLCJsb2NhdGlvbiI6Imh0dHA6Ly93d3cuZ3JhbW1hcmx5LmNvbS9leHRlbnNpb24tc3VjY2VzcyJ9; funnelType=free; browser_info=CHROME:48:COMPUTER:SUPPORTED:FREEMIUM; optimizelySegments=%7B%22299712489%22%3A%22gc%22%2C%22300064087%22%3A%22false%22%2C%22300337320%22%3A%22search%22%2C%22753030770%22%3A%22none%22%2C%224659346861%22%3A%22true%22%2C%224734220892%22%3A%22true%22%2C%224666431804%22%3A%22true%22%2C%224724020596%22%3A%22true%22%7D; optimizelyReferrer=; experiment_groups=001_post_reg_control2|002_post_reg_show; optimizelyBuckets=%7B%225369272632%22%3A%225399830512%22%7D; _ga=GA1.2.2091691156.1459928435; gnar_containerId=DmIkk3XbUl0K; mp_mixpanel__c=2; __fngrprnt__=uUpUxuQHv7KV; mp_c10dd64c87f70ef5563a63c368797e8c_mixpanel=%7B%22distinct_id%22%3A%20%22153ea840982d0-0d8ba8847-3d700057-240000-153ea840983677%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.co.uk%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.co.uk%22%2C%22__mps%22%3A%20%7B%22%24os%22%3A%20%22Linux%22%2C%22%24browser%22%3A%20%22Chrome%22%2C%22%24browser_version%22%3A%2048%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.co.uk%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.co.uk%22%7D%2C%22__mpso%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpap%22%3A%20%5B%5D%7D"
-
+#cookie = "optimizelyEndUserId=oeu1459928410487r0.9973470086697489; gProduct=Chrome-Freemium; _mhbkts=gdn; grauth=AABCBdiDM1QKQgeU-enVbnOQAopyeHsI5OCc_GZTToDnHhLr66ZvgKMro9TKqCUMSUjVhw; isGrammarlyUser=true; _free=true; optimizelySegments=%7B%22299712489%22%3A%22gc%22%2C%22300064087%22%3A%22false%22%2C%22300337320%22%3A%22search%22%2C%22753030770%22%3A%22brand_f1%22%2C%224659346861%22%3A%22true%22%2C%224734220892%22%3A%22true%22%2C%224666431804%22%3A%22true%22%2C%224724020596%22%3A%22true%22%7D; optimizelyReferrer=; optimizelyBuckets=%7B%225369272632%22%3A%225399830512%22%7D; _ga=GA1.2.2091691156.1459928435; gnar_containerId=DmIkk3XbUl0K; mp_c10dd64c87f70ef5563a63c368797e8c_mixpanel=%7B%22distinct_id%22%3A%20440873981%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.co.uk%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.co.uk%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpap%22%3A%20%5B%5D%2C%22utm_source%22%3A%20%22google%22%2C%22utm_medium%22%3A%20%22cpc%22%2C%22utm_campaign%22%3A%20%22brand_f1%22%2C%22utm_content%22%3A%20%2276996511046%22%2C%22utm_term%22%3A%20%22grammarly%22%7D; __fngrprnt__=4BIClOcibJ4r; f=004_referral_control"
 headers = [
     "Host: capi.grammarly.com",
     "Origin: chrome-extension://kbfnbcaeplbcioakkpcpgfkobkghlhen",
-    #"Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits",
-    #"Sec-WebSocket-Key: s2PXpLhNq/0wOXCP3QK2hA==",
-    #"Sec-WebSocket-Version:13",
-    #"User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"
 ]
-
 
 HELLO_MSG = {
     "token": None,
@@ -36,14 +40,25 @@ TEXT_MSG = {
 }
 
 
-def process_message(original_msg):
-    #print("RECEIVED:", original_msg)
-    return json.loads(original_msg)
+def wait_message():
+    """
+    """
+    try:
+        msg = connection.recv()
+    except websocket._exceptions.WebSocketConnectionClosedException as e:
+        print(e)
+        global connection
+        connection = connect()
+        msg = connection.recv()
+    else:
+        print(msg)
+        return json.loads(msg)
 
 def connect():
     """
     Establish a websocket connection, return channel object.
     """
+    global connection
     connection = websocket.create_connection(
         "wss://capi.grammarly.com/freews",
         cookie=cookie,
@@ -51,31 +66,42 @@ def connect():
         #subprotocols=["wamp.2.json"],
         #timeout=settings.TIMEOUT)
     connection.send(json.dumps(HELLO_MSG))
-    msg = connection.recv()
-    msg = process_message(msg)
+    msg = wait_message()
     assert msg["action"] == "start"
     return connection
 
 def evaluate_text(text=TEXT):
+    """
+    Given a text, return its analysis object.
+    The analysis object contains a list of:
+        {
+            "start"  # initial position
+            "selection"  # string of the word or expression with issue
+            "suggestions"  # may be an empty list
+            "explanation"  # optional
+            "originalSymbol"  # how the mistake was categorized by Grammarly
+            "symbol"  # (optional) EFCAM symbol representation of originalSymbol
+        }
+    """
     formatted_text = "+0:0:{}:0".format(text)
     TEXT_MSG["ch"] = [formatted_text]
     connection.send(json.dumps(TEXT_MSG))
-    msg = connection.recv()
-    msg = process_message(msg)
+    msg = wait_message()
     assert msg["action"] == "submit_ot"
     text_id = msg["id"]
     analysis_list = []
     while not msg["action"] == "finished":
-        msg = connection.recv()
-        msg = process_message(msg)
+        msg = wait_message()
         if msg["action"] == "alert":
             analysis = {
                 "start": msg["begin"],
                 "selection": msg["text"],
                 "suggestions": msg["replacements"],
-                "explanation": msg.get("explanation"),
                 "originalSymbol": "{}.{}.{}".format(msg["group"], msg["category"], msg.get("point", ""))
             }
+            explanation = msg.get("explanation")
+            if explanation:
+                analysis["explanation"] = explanation
             original_symbol = analysis["originalSymbol"].lower()
             original_word = text[msg["begin"]:msg["end"]]
             replacement = ""
@@ -91,111 +117,50 @@ def evaluate_text(text=TEXT):
                 analysis_list.append(analysis)
     return analysis_list
 
-connection = connect()
-evaluation = evaluate_text()
-print(evaluation)
-print(len(evaluation))
 
 
+if __name__ == "__main__":
+    #text = TEXT
+    #print(text)
+    #evaluation = evaluate_text(text)
+    #print(evaluation)
+    #print(len(evaluation))
+    global connection
+    connection = connect()
 
-# 1. LOCAL -> SERVER: HELLO_MSG
-# 2. LOCAL -> SERVER: TEXT_MSG
-# 3. SERVER -> LOCAL: START {"sid":16933119,"action":"start","id":0}
-# 4. SERVER -> LOCAL: SUBMIT OT {"rev":0,"action":"submit_ot","id":1}
-# 5. SERVER -> LOCAL:
-
-{
-    "point": "General",
-    "transforms": [
-        "\u003cspan class\u003d\u0027gr_grammar_del\u0027\u003etatiana\u003c/span\u003e\u003cspan class\u003d\u0027gr_grammar_ins\u0027\u003eTatiana\u003c/span\u003e",
-        "\u003cspan class\u003d\u0027gr_grammar_del\u0027\u003etatiana\u003c/span\u003e\u003cspan class\u003d\u0027gr_grammar_ins\u0027\u003eTatyana\u003c/span\u003e",
-        "\u003cspan class\u003d\u0027gr_grammar_del\u0027\u003etatiana\u003c/span\u003e\u003cspan class\u003d\u0027gr_grammar_ins\u0027\u003eTiana\u003c/span\u003e"
-    ],
-    "title": "Misspelled word: \u003ci\u003etatiana\u003c/i\u003e",
-    "details": "",
-    "explanation": "\u003cp\u003eThe word \u003cb\u003etatiana\u003c/b\u003e\u0026nbsp;is not in\u0026nbsp;our dictionary. You can add it\u0026nbsp;to\u0026nbsp;your personal dictionary to\u0026nbsp;prevent future alerts.\n",
-    "examples": "\u003cp\u003echange spelling\n",
-    "todo": "",
-    "handbookLink": "",
-    "sentence_no": 0,
-    "free": True,
-    "cardLayout": {
-        "category": "Correctness",
-        "group": "Spelling",
-        "groupDescription": "Review the following words for misspellings"
-    },
-    "category": "Misspelled",
-    "pid": 455929,
-    "rid": 455929,
-    "begin": 201,
-    "end": 208,
-    "text": "tatiana",
-    "group": "Spelling",
-    "rev": 0,
-    "highlightBegin": 201,
-    "highlightEnd": 208,
-    "highlightText": "tatiana",
-    "replacements": ["Tatiana","Tatyana","Tiana"],
-    "extra_properties" : {
-        "priority": "1"
-    },
-    "action":"alert",
-    "id":17
-}
-
-{
-    "point": "General",
-    "transforms": [
-        "\u003cspan class\u003d\u0027gr_grammar_del\u0027\u003eChueyr\u003c/span\u003e\u003cspan class\u003d\u0027gr_grammar_ins\u0027\u003eChuey\u003c/span\u003e",
-        "\u003cspan class\u003d\u0027gr_grammar_del\u0027\u003eChueyr\u003c/span\u003e\u003cspan class\u003d\u0027gr_grammar_ins\u0027\u003eChur\u003c/span\u003e"
-    ],
-    "title": "Misspelled word: \u003ci\u003eChueyr\u003c/i\u003e",
-    "details": "",
-    "explanation": "\u003cp\u003eThe word \u003cb\u003eChueyr\u003c/b\u003e\u0026nbsp;is not in\u0026nbsp;our dictionary. You can add it\u0026nbsp;to\u0026nbsp;your personal dictionary to\u0026nbsp;prevent future alerts.\n","examples":"\u003cp\u003echange spelling\n",
-    "todo": "",
-    "handbookLink": "",
-    "sentence_no": 0,
-    "free": True,
-    "cardLayout": {
-        "category": "Correctness",
-        "group": "Spelling",
-        "groupDescription": "Review the following words for misspellings"
-    },
-    "category":"Misspelled",
-    "pid": 893584,
-    "rid": 455929,
-    "begin": 13,
-    "end":19,
-    "text": "Chueyr",
-    "group": "Spelling",
-    "rev": 0,
-    "highlightBegin": 13,
-    "highlightEnd": 19,
-    "highlightText": "Chueyr",
-    "replacements": ["Chuey","Chur"],
-    "extra_properties": {
-        "priority": "1"
-    },
-    "action": "alert",
-    "id": 18
-}
-
-{
-    "rev": 0,
-    "scores": {
-        "Vocabulary": 87.87878787878788,
-        "PassiveIndex": 100.0,
-        "Conciseness": 100.0,
-        "Correctness": 20.0,
-        "Clarity": 55.4
-    },
-    "score": 20,
-    "removed": [],
-    "errors": 0,
-    "interrupts": 0,
-    "skipped": 0,
-    "rejected": 0,
-    "blocked": 0,
-    "dialect": "undefined",
-    "action": "finished"
-}
+    with open("../../../data/299.txt") as in_fp:
+        with open("../../../data/299-grammarly-p10.txt", "w") as out_fp:
+            # 310: some problem
+            # 605: another problem
+            # 712: another problem
+            # 3172: after it there was an
+            # ssl.SSLError: [SSL: TLSV1_ALERT_INTERNAL_ERROR] tlsv1 alert internal error (_ssl.c:645)
+            # 7692
+            #min_i = 8127
+            #min_i = 8130
+            min_i = 8649
+            i = 0
+            for line in in_fp:
+                if i >= min_i:
+                    print("<<<<<<<<<<<<<<<<<<", i, ">>>>>>>>>>>>>>>>")
+                    print(datetime.now().isoformat())
+                    data = json.loads(line)
+                    try:
+                        data["changes"] = evaluate_text(data["text"])
+                    except Exception as e:
+                        print("EXCEPTION!!!! ", i)
+                        print(e)
+                    else:
+                        out_fp.write("{}\n".format(json.dumps(data)))
+                    try:
+                        connection.close()
+                    except websocket._exceptions.WebSocketConnectionClosedException:
+                        pass
+                    connection = False
+                    while not connection:
+                        try:
+                            connection = connect()
+                        except ssl.SSLError as e:
+                            print(e)
+                            time.sleep(2)
+                i += 1
