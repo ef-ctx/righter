c14e2a556d16cad250226190509b660c217fceab
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2016-09-12 08:33:24 +0100

    Fix errors on flatten

--- tests/unit/test_dictionary.py
+++ tests/unit/test_dictionary.py
@@ -6,5 +6,5 @@ class DictionaryTestCase(unittest.TestCase):
 
     def test_words(self):
         self.assertTrue(isinstance(dictionary.WORDS, set))
-        self.assertEqual(len(dictionary.WORDS), 663016)
+        self.assertEqual(len(dictionary.WORDS), 350241)
         self.assertIn("platypus", dictionary.WORDS)
